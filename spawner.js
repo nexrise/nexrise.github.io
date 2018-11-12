b026f568f5b3fcec4f4aa1126f7037ed2908d358
mkhobaib <kkhhbb.91@gmail.com>
2018-11-12 22:05:06 +0300

    new changes

--- spawner.js
+++ spawner.js
@@ -41,6 +41,7 @@ document.getElementById("menutoggle").innerHTML =
 "<a id='event' href='events.html'>Events</a>" +
 "<div id='togglee' class='a dropdown-content'>" +
 
+"<a href='ce8.html'>AUGMENTED AND VIRTUAL REALITY 2019<br>MARCH 07 & 08, 2019 | INTERCONTINENTAL HOTEL<br>KUALA LUMPUR - MALAYSIA</a>" +
 "<a href='ce7.html'>VAT: NATIONAL ASPECTS 2019<br>NOVEMBER 22, 2018 | GULF HOTEL<br>KINGDOM OF BAHRAIN</a>" +
 "<a href='ce6.html'>TEACHING AND LEARNING CONCLAVE 2019<br>February 14-15, 2019 | Radisson Blu Hotel<br>Addis Ababa - Ethiopia</a>" +
 "<a href='ce5.html'>KINGDOM EDUCATIONAL SUMMIT 2019<br>January 23-24, 2019 | Gulf Hotel<br>Kingdom of Bahrain</a>" +	
